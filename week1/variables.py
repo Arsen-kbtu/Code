81ff060f0a493f986cfb1b788c019f95832a686c
John Doe <a_zhexenbiyev@kbtu.kz>
2023-01-19 12:16:04 +0600

    dgfsd

--- week1/variables.py
+++ week1/variables.py
@@ -1,2 +1,22 @@
-x =5
-y= "hello world"
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+print(type(x))
+print(type(y))
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
